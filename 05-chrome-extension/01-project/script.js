8c9b8f69003f65968712c36700899761feebb4c9
Agustín Comolli <agustincomolli@gmail.com>
2025-08-01 11:28:48 -0300

    Add render function, and improve creation of leads items. Use template strings

--- 05-chrome-extension/01-project/script.js
+++ 05-chrome-extension/01-project/script.js
@@ -3,7 +3,7 @@
 //  inputEl -> should be assigned to the text input field
 const saveButton = document.querySelector("#save");
 const newLead = document.querySelector("#new-lead");
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 // Grab the unordered list and store it in a const variable called ulEl
 const leadList = document.querySelector("#lead-list");
 
@@ -12,12 +12,33 @@ const leadList = document.querySelector("#lead-list");
 // Google -> "get value from input field javascript"
 function save() {
   myLeads.push(newLead.value);
-  console.log(myLeads);
+  // Clear out the input field
+  newLead.value = "";
+  renderLeads();
 }
 
 saveButton.addEventListener("click", save);
 
-// Render the leads in the unordered list using ulEl.textContent
-for (let i = 0; i < myLeads.length; i++) {
-  leadList.innerHTML += "<li>" + myLeads[i] + "</li>";
+function renderLeads() {
+  // Create a variable, listItems, to hold all the HTML for the list items
+  // Assign it to an empty string to begin with
+  let listItems = "";
+
+  for (let i = 0; i < myLeads.length; i++) {
+    // leadList.innerHTML += "<li>" + myLeads[i] + "</li>";
+
+    // Otra forma de hacerlo:
+    //  const listItem = document.createElement("li");
+    //  listItem.textContent = myLeads[i];
+    //  leadList.append(listItem);
+
+    // Best performance way
+    listItems += `
+      <li>
+        <a href="https://${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+      </li>`;
+  };
+
+  // Render the listItems inside the unordered list using ulEl.innerHTML
+  leadList.innerHTML = listItems;
 };

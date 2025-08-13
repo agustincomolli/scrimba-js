262d729f26e1b20dd85b2798d252d5d57f4eec61
Agustín Comolli <agustincomolli@gmail.com>
2025-08-13 12:56:58 -0300

    finish chrome extension

--- 05-chrome-extension/01-project/script.js
+++ 05-chrome-extension/01-project/script.js
@@ -3,6 +3,7 @@
 //  inputEl -> should be assigned to the text input field
 let myLeads = JSON.parse(localStorage.getItem("leads")) || [];
 const saveButton = document.querySelector("#save");
+const saveTabButton = document.querySelector("#save-tab");
 const deleteButton = document.querySelector("#delete");
 const newLead = document.querySelector("#new-lead");
 // Grab the unordered list and store it in a const variable called ulEl
@@ -11,14 +12,24 @@ const leadList = document.querySelector("#lead-list");
 // Push the value from the inputEl into the myLeads array 
 // instead of the hard-coded "www.awesomeleads.com" value
 // Google -> "get value from input field javascript"
-function save() {
-  myLeads.push(newLead.value);
+function save(value) {
+  myLeads.push(value);
   // Save the myLeads array to localStorage 
   localStorage.setItem("leads", JSON.stringify(myLeads));
   // Clear out the input field
-  newLead.value = "";
   render(myLeads);
 };
+
+function saveInputLead() {
+  save(newLead.value);
+  newLead.value = "";
+};
+
+function saveTab() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    save(tabs[0].url);
+  });
+}
 
 function deleteAll() {
   localStorage.clear();
@@ -50,7 +61,8 @@ function render(leads) {
   leadList.innerHTML = listItems;
 };
 
-saveButton.addEventListener("click", save);
+saveButton.addEventListener("click", saveInputLead);
+saveTabButton.addEventListener("click", saveTab);
 // Listen for double clicks on the delete button (google it!)
 deleteButton.addEventListener("dblclick", deleteAll);
 
